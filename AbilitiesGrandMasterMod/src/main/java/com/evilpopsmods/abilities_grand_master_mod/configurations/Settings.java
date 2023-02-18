119345e0d4eb7c418b58feaf1bf80849592e2c00
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2023-02-18 03:10:13 +0100

    [#4] Fighting skill - bugfix - In 'Settings' class database path is now set from "../..." to "./..."

--- AbilitiesGrandMasterMod/src/main/java/com/evilpopsmods/abilities_grand_master_mod/configurations/Settings.java
+++ AbilitiesGrandMasterMod/src/main/java/com/evilpopsmods/abilities_grand_master_mod/configurations/Settings.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class Settings {
     public static final Path PLAYERS_SKILL_DATA_PATH_OBJ = Paths.get(Settings.PLAYERS_SKILLS_DATA_FILE_PATH);
-    public static final String PLAYERS_SKILLS_DATA_FILE_PATH = "../" + Settings.MOD_FOLDER_NAME + "/" + Settings.PLAYERS_SKILLS_DATA_FILE_NAME;
+    public static final String PLAYERS_SKILLS_DATA_FILE_PATH = "./" + Settings.MOD_FOLDER_NAME + "/" + Settings.PLAYERS_SKILLS_DATA_FILE_NAME;
     private static final String MOD_FOLDER_NAME = "AbilitiesGrandMasterModData";
     private static final String PLAYERS_SKILLS_DATA_FILE_NAME = "players_skills_data.txt";
 
